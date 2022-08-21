532d5645cc29abb4101868b532552c695ec3e5d2
FreeElixir <Mazen3722@gmail.com>
2022-08-21 06:12:46 +0200

    ghaleb edits

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: [ "Developers", "Graphic Designers" , "Editors"],
+        strings: [ "Business developers", "Graphic Designers" , "Editors" , "animators" , "Video Producers"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: [ "Developers", "Graphic Designers" , "Editors"],
+        strings: [ "Business developers", "Graphic Designers" , "Editors" , "animators" , "Video Producers"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
